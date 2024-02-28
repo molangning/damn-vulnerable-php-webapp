aefe7e3793078d442e0a692ca3737d79ec4a0020
Mo Langning <133737702+molangning@users.noreply.github.com>
2024-02-28 04:13:45 +0000

    first commit!!!11!1

---
+++ run.sh
@@ -0,0 +1,4 @@
+trap "trap - SIGTERM && kill -- -$$" SIGINT SIGTERM EXIT
+
+./cloudflared-linux-amd64 tunnel --url http://localhost:8000 &> cloudflared-logs.txt &
+php -S localhost:8000
---
+++ setup.sh
@@ -0,0 +1,2 @@
+wget https://github.com/cloudflare/cloudflared/releases/latest/download/cloudflared-linux-amd64
+chmod +X cloudflared-linux-amd64
